7118d30d023e1ef1c40f97d3c12cd30087075f0d
Herobrine2008 <44743108+Herobrine2008@users.noreply.github.com>
2018-12-21 16:37:02 +0800

    Create Thirth Program.cpp

---
+++ Thirth Program.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a;
+  float b;
+  string c;
+  double d;
+  long long e;
+  char f;
+  cin>>a>>b>>c>>d>>e>>f;
+  cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<e<<' '<<f<<endl;  //Output all things!
+  return 0;
